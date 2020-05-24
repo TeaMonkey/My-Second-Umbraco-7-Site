554bf32acdb3173f5ff24ba24f61588173d1cd04
TeaMonkey <TeaMonkey@users.noreply.github.com>
2020-05-24 17:40:47 +0100

    Added some partial views to break down the layout

---
+++ _src/My Second Umbraco 7 Site/My Second Umbraco 7 Site/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+
+namespace My_Second_Umbraco_7_Site.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        private const string PartialViewDir = "~/Views/Partials/Home/";
+
+        public ActionResult RenderFeatured()
+        {
+            return PartialView($"{PartialViewDir}_Featured.cshtml");
+        }
+
+        public ActionResult RenderServices()
+        {
+            return PartialView($"{PartialViewDir}_Services.cshtml");
+        }
+    }
+}
---
+++ _src/My Second Umbraco 7 Site/My Second Umbraco 7 Site/Controllers/SiteLayoutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace My_Second_Umbraco_7_Site.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        private const string PartialViewDir = "~/Views/Partials/SiteLayout/";
+
+        public ActionResult RenderHeader()
+        {
+            return PartialView($"{PartialViewDir}_Header.cshtml");
+        }
+
+        public ActionResult RenderFooter()
+        {
+            return PartialView($"{PartialViewDir}_Footer.cshtml");
+        }
+
+        public ActionResult RenderIntro()
+        {
+            return PartialView($"{PartialViewDir}_Intro.cshtml");
+        }
+    }
+}

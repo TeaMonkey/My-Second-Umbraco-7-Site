3d93a57dc4c8ad1c15c6e41423ef8ed2e97461a4
TeaMonkey <TeaMonkey@users.noreply.github.com>
2020-06-21 11:11:46 +0100

    Added smaller blog layout support and added first part of Instagram section controls 2

---
+++ _src/My Second Umbraco 7 Site/My Second Umbraco 7 Site/Models/BlogItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace My_Second_Umbraco_7_Site.Models
+{
+    public class BlogItem
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Intro { get; set; }
+        public DateTime PostDate { get; set; }
+        public string LinkUrl { get; set; }
+
+        public BlogItem(string name, string category, string intro, DateTime postDate, string linkUrl)
+        {
+            Name = name;
+            Category = category;
+            Intro = intro;
+            PostDate = postDate;
+            LinkUrl = linkUrl;
+        }
+    }
+}
---
+++ _src/My Second Umbraco 7 Site/My Second Umbraco 7 Site/Models/Instagram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace My_Second_Umbraco_7_Site.Models
+{
+    public class Instagram
+    {
+        public string Title { get; set; }
+
+        public Instagram(string title)
+        {
+            Title = title;
+        }
+    }
+}
